104b255f030eacc6366df39b3e956208d9f9366c
1DanielGe1 <danielge0608@gmail.com>
2019-03-13 00:07:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jianpeng/mooddiary/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.jianpeng.mooddiary;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView bottomNavigationView;
+    private  HomeFragment home_fragment;
+    private CalendarFragment calendar_fragment;
+    private NewsFragment news_fragment;
+    private MeFragment me_fragment;
+    private Fragment[] fragments;
+    private int lastfragment;//用于记录上个选择的Fragment
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFragment();
+    }
+
+    private void initFragment() {
+        home_fragment = new HomeFragment();
+        calendar_fragment = new CalendarFragment();
+        news_fragment = new NewsFragment();
+        me_fragment=new MeFragment();
+        fragments = new Fragment[]{home_fragment,calendar_fragment,news_fragment, me_fragment};
+        lastfragment=0;
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainview,home_fragment).show(home_fragment).commit();
+        bottomNavigationView=(BottomNavigationView)findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(changeFragment);
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener changeFragment
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                {
+                    if(lastfragment!=0)
+                    {
+                        switchFragment(lastfragment,0);
+                        lastfragment=0;
+                    }
+                    return true;
+                }
+                case R.id.navigation_calendar:
+                {
+                    if(lastfragment!=1)
+                    {
+                        switchFragment(lastfragment,1);
+                        lastfragment=1;
+                    }
+                    return true;
+                }
+                case R.id.navigation_news:
+                {
+                    if(lastfragment!=2)
+                    {
+                        switchFragment(lastfragment,2);
+                        lastfragment=2;
+                    }
+                    return true;
+                }
+                case R.id.navigation_me:
+                {
+                    if(lastfragment!=3)
+                    {
+                        switchFragment(lastfragment,3);
+                        lastfragment=3;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+
+
+    //切换Fragment
+    private void switchFragment(int lastfragment,int index)
+    {
+        FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+        transaction.hide(fragments[lastfragment]);//隐藏上个Fragment
+        if(fragments[index].isAdded()==false)
+        {
+            transaction.add(R.id.mainview,fragments[index]);
+        }
+        transaction.show(fragments[index]).commitAllowingStateLoss();
+    }
+
+
+}
